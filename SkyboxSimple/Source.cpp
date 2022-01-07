0068c15657528545be95de71352917479257ad4a
sreerag zreeeezzzz <sreeragzreeezzz@gmail.com>
2022-01-07 12:29:27 +0530

    Skybox with cube and reflection

--- SkyboxSimple/Source.cpp
+++ SkyboxSimple/Source.cpp
@@ -1,4 +1,5 @@
-
+//Reflection and refraction of skybox texture on 3d objects
+//for reflection and refraction only need to change from the frag shader of the object
 #include<GL/glew.h>
 #include<GLFW/glfw3.h>
 #include<iostream>
@@ -14,6 +15,8 @@
 using namespace std;
 
 void MouseCallback(GLFWwindow* window, double xPos, double yPos);
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void DoMovement();
 
 GLint wid = 2000; //*window variable
 GLint height1 = 1500; //*window variable
@@ -29,7 +32,7 @@ float lastFrame = 0.0f;
 
 GLfloat lastX = 400;
 GLfloat lastY = 300;
-//bool keys[1024];
+bool keys[1024];
 bool firstMouse = true;
 
 void main()
@@ -48,6 +51,7 @@ void main()
 
 	glfwMakeContextCurrent(window);
 	glfwGetFramebufferSize(window, &screen_width, &screen_height);
+	glfwSetKeyCallback(window, KeyCallback);
 	glfwSetCursorPosCallback(window, MouseCallback);
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
@@ -63,11 +67,57 @@ void main()
 	glEnable(GL_DEPTH_TEST);
 	Shader shaders("skybox.vs", "skybox.frag");
 
+	Shader shaders1("cube.vs", "cube.frag");
 
+	GLfloat cube[] =
+	{
+		 -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+	 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+	 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+	-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+	-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+	-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+	-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+	-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+	 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+
+	};
 
 	GLfloat skybox[] =
 	{
-		// Positions
+		 //Positions
 		-1.0f,  1.0f, -1.0f,
 		-1.0f, -1.0f, -1.0f,
 		1.0f, -1.0f, -1.0f,
@@ -112,6 +162,25 @@ void main()
 
 	};
 
+
+	//cube
+	GLuint cubeVAO, cubeVBO;
+	glGenVertexArrays(1, &cubeVAO);
+	glGenBuffers(1, &cubeVBO);
+	glBindVertexArray(cubeVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
+	//vertices
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	//normal
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	//skybox
 	GLuint skyboxVAO, skyboxVBO;
 	glGenVertexArrays(1, &skyboxVBO);
 	glGenBuffers(1, &skyboxVAO);
@@ -125,14 +194,14 @@ void main()
 	glBindVertexArray(0);
 
 	//skybox cube mapping
-	// Cubemap (Skybox)
+	//Cubemap (Skybox)
 	vector<const GLchar*> faces;
-	faces.push_back("res/images/right.png");
-	faces.push_back("res/images/left.png");
-	faces.push_back("res/images/top.png");
-	faces.push_back("res/images/bottom.png");
-	faces.push_back("res/images/back.png");
-	faces.push_back("res/images/front.png");
+	faces.push_back("res/images/Sky/right.png");
+	faces.push_back("res/images/Sky/left.png");
+	faces.push_back("res/images/Sky/top.png");
+	faces.push_back("res/images/Sky/bottom.png");
+	faces.push_back("res/images/Sky/back.png");
+	faces.push_back("res/images/Sky/front.png");
 	GLuint cubemapTexture = TextureLoading::LoadCubemap(faces);
 
 
@@ -147,32 +216,95 @@ void main()
 		glClearColor(0, 0, 0, 0);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glfwPollEvents();
-		glm::mat4 view1 = camera.GetViewMatrix();
-		glm::mat4 model1;
-		
-
+		DoMovement();
 		//skybox
-		glm::mat4 view;
-		//glDepthFunc(GL_EQUAL);
+		glm::mat4 view1 = camera.GetViewMatrix();
+		glDepthFunc(GL_EQUAL);
 		shaders.Use();
-		view = glm::mat4(glm::mat3(camera.GetViewMatrix()));
-		glUniformMatrix4fv(glGetUniformLocation(shaders.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		view1 = glm::mat4(glm::mat3(camera.GetViewMatrix()));
+		glUniformMatrix4fv(glGetUniformLocation(shaders.Program, "view"), 1, GL_FALSE, glm::value_ptr(view1));
 		glUniformMatrix4fv(glGetUniformLocation(shaders.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 		glBindVertexArray(skyboxVAO);
 		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glBindVertexArray(0);
-		glDepthFunc(GL_LESS);
-		glDepthFunc(GL_EQUAL);
+		glDepthFunc(GL_LEQUAL);
+
+		//cube
+		glm::mat4 view2 = camera.GetViewMatrix();
+		glm::mat4 model2;
+		shaders1.Use();
+		
+		GLint modellocation = glGetUniformLocation(shaders1.Program, "model");
+		GLint viewlocation = glGetUniformLocation(shaders1.Program, "view");
+		GLint projectionlocation = glGetUniformLocation(shaders1.Program, "projection");
+		glUniformMatrix4fv(viewlocation, 1, GL_FALSE, glm::value_ptr(view2));
+		glUniformMatrix4fv(projectionlocation, 1, GL_FALSE, glm::value_ptr(projection));
+		glBindVertexArray(cubeVAO);
+		glUniformMatrix4fv(modellocation, 1, GL_FALSE, glm::value_ptr(model2));
+		glBindVertexArray(cubeVAO);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		
 		glfwSwapBuffers(window);
 		
 	}
-	/*glDeleteVertexArrays(1, &cubeBuf2);
-	glDeleteVertexArrays(1, &skyboxBuf2);
-	glDeleteBuffers(1, &cubeBuf1);
-	glDeleteBuffers(1, &skyboxBuf1);*/
 	glfwTerminate();
 }
+
+// Moves/alters the camera positions based on user input
+void DoMovement()
+{
+	// Camera controls
+	if (keys[GLFW_KEY_W] || keys[GLFW_KEY_UP])
+	{
+		cout << "w key pressed\n";
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+
+	}
+
+	if (keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN])
+	{
+		cout << "s key pressed\n";
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	}
+
+	if (keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT])
+	{
+		cout << "a key pressed\n";
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	}
+
+	if (keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT])
+	{
+		cout << "d key pressed\n";
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+	}
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (GLFW_KEY_ESCAPE == key && GLFW_PRESS == action)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+		{
+			keys[key] = true;
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			keys[key] = false;
+		}
+	}
+}
+
+
 
 void MouseCallback(GLFWwindow* window, double xPos, double yPos)
 {
---
+++ SkyboxSimple/cube.vs
@@ -0,0 +1,17 @@
+#version 330 core
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normaldata ;
+
+out vec3 normal;
+out vec3 pos;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+    normal=mat3(transpose(inverse(model)))*normaldata;
+    pos=vec3(model*vec4(position,1.0));
+    gl_Position = projection * view * model * vec4(pos, 1.0f);
+}
